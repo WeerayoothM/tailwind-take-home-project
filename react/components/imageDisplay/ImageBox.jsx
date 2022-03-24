01285cf22e7d4a48b38912dca579143aff889b4d
WeerayoothM <weerayooth.ma@gmail.com>
2022-03-24 10:02:22 +0700

    add sticky when scroll

--- react/components/imageDisplay/ImageBox.jsx
+++ react/components/imageDisplay/ImageBox.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ImageBox({ src = "", style = "" }) {
   return (
     <div
-      tabindex="0"
+      tabIndex="0"
       className={`w-full rounded-lg border border-gray-200 hover:border-gray-400 focus:border-transparent focus:shadow-outline-gray ${style}`}
     >
       <img src={src} alt={src.split(".")[0]} className="w-full rounded-lg  " />
--- react/components/imageDisplay/ImageDisplay.jsx
+++ react/components/imageDisplay/ImageDisplay.jsx
@@ -3,12 +3,14 @@ import ImageBox from "./ImageBox";
 
 function ImageDisplay() {
   return (
-    <div className="w-full">
-      <ImageBox src={"/img/head-black_front.png"} />
-      <div className="grid grid-cols-3 gap-4 mt-3">
+    <div className="h-full relative">
+      <div className="md:sticky w-full inset-0 top-8">
         <ImageBox src={"/img/head-black_front.png"} />
-        <ImageBox src={"/img/head-black_front-side-left.png"} />
-        <ImageBox src={"/img/powerhead_back.png"} />
+        <div className="grid grid-cols-3 gap-4 mt-3">
+          <ImageBox src={"/img/head-black_front.png"} />
+          <ImageBox src={"/img/head-black_front-side-left.png"} />
+          <ImageBox src={"/img/powerhead_back.png"} />
+        </div>
       </div>
     </div>
   );
